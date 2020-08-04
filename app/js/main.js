28f3505d10778941dc64fb43787200c4a734ec74
unknown <jet333@yandex.ru>
2020-08-04 23:03:46 +0300

    верстаем секцию surf

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,17 @@ $(function(){
 
 $('.header__slider').slick({
   infinite: true,
+  fade: true,
+  prevArrow: '<img class="slider-arrows slider-arrows__left slick-prev" src="img/arrow-left.svg">',
+  nextArrow: '<img class="slider-arrows slider-arrows__right slick-next" src="img/arrow-right.svg">',
+  asNavFor: '.slider-dots'
 });
 
+$('.slider-dots').slick({
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  arrows: false,
+  asNavFor: '.header__slider'
+})
   
 });
