15bc6ec935d2ac29f39dfc493262f97692d14197
unknown <jet333@yandex.ru>
2020-08-09 12:44:25 +0300

    section surf

--- app/js/main.js
+++ app/js/main.js
@@ -13,6 +13,30 @@ $('.slider-dots').slick({
   slidesToScroll: 4,
   arrows: false,
   asNavFor: '.header__slider'
-})
+});
   
+$('.surf-slider').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  asNavFor: '.slider-map',
+  prevArrow: '<img class="slider-arrows slider-arrows__left slick-prev" src="img/arrow-left.svg">',
+  nextArrow: '<img class="slider-arrows slider-arrows__right slick-next" src="img/arrow-right.svg">'
+});
+
+$('.slider-map').slick ({
+  slidesToShow: 8,
+  slidesToScroll: 1,
+  arrows: false,
+  asNavFor: '.surf-slider',
+  focusOnSelect: true,
+
+});
+
+$('.slider-dotsmap').slick ({
+  slidesToShow: 8,
+  slidesToScroll: 1,
+  arrows: false,
+  asNavFor: '.slider-map'
+
+});
 });
